5681f9d7483b688ca3d0355f664ac8801c949525
Konstantyn Golik <Kos505@mail.ru>
2023-11-28 19:22:59 +0700

    Добавлена и решена 3 задача

--- Program.cs
+++ Program.cs
@@ -62,5 +62,38 @@
 //     return sum + SumNaturalNumbers(start + 1, end);
 // }
 
+// Задача 68: Напишите программу вычисления
+// функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 29
 
+
+// // Console.WriteLine($"Задайте неотрицательное число m ");
+// // int m = Convert.ToInt32(Console.ReadLine());
+// // Console.WriteLine($"Задайте неотрицательное число n ");
+// // int n = Convert.ToInt32(Console.ReadLine());
+// // Console.WriteLine($"{AckermanFunction(m, n)}");
+
+// int m = 2;
+// int n = 3;
+// Console.WriteLine($"{AckermanFunction(m, n)}");
+
+// int AckermanFunction(int m, int n)
+// {
+//     if (m == 0)
+//     {
+//         return n + 1;
+//     }
+//     else
+//     {
+//         if ((m != 0) && (n == 0))
+//         {
+//             return AckermanFunction(m - 1, 1);
+//         }
+//         else
+//         {
+//             return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+//         }
+//     }
+// }
 
